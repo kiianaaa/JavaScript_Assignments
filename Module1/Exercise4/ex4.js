bea3398b286d1a072ad86834182b8845267c8b20
kiianaaa <kiana.aghajani@metropolia.fi>
2022-11-17 10:27:33 +0200

    ex8

--- Module1/Exercise4/ex4.js
+++ Module1/Exercise4/ex4.js
@@ -3,19 +3,35 @@
 let name = prompt(`Give me your name child`);
 
 let house = (Math.floor(Math.random() * 4) + 1); 
+console.log(house);
 
-if (house == 1) {
-    document.querySelector('.house').innerHTML = (`You are a Gryffindor ${name}!`);
-}
+//if (house == 1) {
+  //  document.querySelector('.house').innerHTML = (`You are a Gryffindor ${name}!`);
+//}
 
-if (house == 2) {
-    document.querySelector('.house').innerHTML = (`You are a Hufflepuff ${name}!`);
-}
+//if (house == 2) {
+  //  document.querySelector('.house').innerHTML = (`You are a Hufflepuff ${name}!`);
+//}
 
-if (house == 3) {
-    document.querySelector('.house').innerHTML = (`You are a Ravenclaw ${name}!`);
-}
+//if (house == 3) {
+  //  document.querySelector('.house').innerHTML = (`You are a Ravenclaw ${name}!`);
+//}
 
-if (house == 4) {
-    document.querySelector('.house').innerHTML = (`You are a Ravenclaw ${name}!`);
+//if (house == 4) {
+  //  document.querySelector('.house').innerHTML = (`You are a Slytherin ${name}!`);
+//}
+
+switch(house) {
+    case 1 :
+        document.querySelector('.house').innerHTML = (`You are a Gryffindor ${name}!`);
+        break;
+    case 2:
+        document.querySelector('.house').innerHTML = (`You are a Hufflepuff ${name}!`);
+        break;
+    case 3:
+        document.querySelector('.house').innerHTML = (`You are a Ravenclaw ${name}!`);
+        break;
+    case 4:
+        document.querySelector('.house').innerHTML = (`You are a Slytherin ${name}!`);
+        break;
 }
---
+++ Module1/Exercise8/ex8.js
@@ -0,0 +1,4 @@
+'use strict';
+
+let start = prompt(`The year you want to start with:`);
+let end = prompt(`The year you want to end with:`);
