eaa1b9f58ecd08cc45c405d9e3a0e294f3fc3ffb
kiianaaa <kiana.aghajani@metropolia.fi>
2022-11-17 11:18:28 +0200

    ex8

--- Module1/Exercise8/ex8.js
+++ Module1/Exercise8/ex8.js
@@ -1,4 +1,16 @@
 'use strict';
 
-let start = prompt(`The year you want to start with:`);
-let end = prompt(`The year you want to end with:`);
+
+var start = parseInt(prompt(`The year you want to start with:`));
+var end = parseInt(prompt(`The year you want to end with:`));
+var ul = document.createElement('ul');
+
+while (start <= end) {
+    if (start % 4 === 0 && start % 100 !== 0 || start % 400 === 0) {
+        var li = document.createElement('li');
+        li.innerHTML = start;
+        ul.appendChild(li);
+    }
+    start++;
+}
+document.getElementById('list').appendChild(ul);
