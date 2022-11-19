7d70de0991f6a9f42b958c973a98e0462bfa3254
kiianaaa <kiana.aghajani@metropolia.fi>
2022-11-19 20:16:54 +0200

    Done

--- Module2/Exercise2/ex2.js
+++ Module2/Exercise2/ex2.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const ppl = parseInt(prompt(`How many people are attending?`));
+let parts = [];
+ 
+for (let i = 0; i < ppl; i++) {
+    let name = prompt("Enter the names of participants:");
+    parts.push(name);
+}
+parts.sort();
+
+var el = document.getElementById('list');
+var ol = document.createElement('ol');
+el.appendChild(ol);
+parts.forEach(function (part) {
+  var f = document.createElement('li');
+  f.appendChild(document.createTextNode(part));
+  ol.appendChild(f);
+});
