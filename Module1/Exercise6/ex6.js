d13513651c67dedeb5d4e5407827563c7d1a9743
kiianaaa <kiana.aghajani@metropolia.fi>
2022-11-19 16:32:27 +0200

    ex6

--- Module1/Exercise6/ex6.js
+++ Module1/Exercise6/ex6.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const answer = confirm(`Should I calculate the square root?`);
+
+if (answer === true) {
+    const number = parseInt(prompt(`Give me a number then!`));
+    let root = number **2;
+    document.querySelector(`.root`).innerHTML = (`The square root of that number is: ${root}`);
+        if (number < 0) {
+            document.querySelector(`.root`).innerHTML = (`The square root of a negative number is not defined`);
+        }
+}
