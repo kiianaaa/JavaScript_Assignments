c8437993c80473c2fc21a70e68649af544456c6d
kiianaaa <kiana.aghajani@metropolia.fi>
2022-11-19 19:55:37 +0200

    Done

--- Module2/Exercise1/ex1.js
+++ Module2/Exercise1/ex1.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const num1 = parseInt(prompt(`Pleae enter the first number:`));
+const num2 = parseInt(prompt(`Pleae enter the second number:`));
+const num3 = parseInt(prompt(`Pleae enter the third number:`));
+const num4 = parseInt(prompt(`Pleae enter the forth number:`));
+const num5 = parseInt(prompt(`Pleae enter the fifth number:`));
+
+let numList = [];
+numList.push(num1);
+numList.push(num2);
+numList.push(num3);
+numList.push(num4);
+numList.push(num5);
+
+for (let index = numList.length - 1; index >= 0; index--) {
+    const element = numList[index];
+    console.log(element);
+  }
