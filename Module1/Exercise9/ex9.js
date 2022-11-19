e4817e360f1c43a63d919a8ba5128ef5b5991311
kiianaaa <kiana.aghajani@metropolia.fi>
2022-11-19 17:20:03 +0200

    ex9

--- Module1/Exercise9/ex9.js
+++ Module1/Exercise9/ex9.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const numb = parseInt(prompt(`Enter the number you want to check:`));
+
+if (numb === 1) {
+    document.querySelector(`.prime`).innerHTML = (`It seems like 1 should be prime, bit it's not`);
+}
+else if (numb === 0) {
+    document.querySelector(`.prime`).innerHTML = (`Zero is neither prime nor composite`);
+}
+else if (numb === 2) {
+    document.querySelector(`.prime`).innerHTML = (`2 is interesting because it is the only even prime number!`);
+}
+else {
+    let isPrime = true;
+    for (let i = 3; i <= Math.ceil(Math.sqrt(numb)); i++) {
+        if (numb % i === 0) {
+            isPrime = false;
+        }
+        if (isPrime === false) {
+            document.querySelector(`.prime`).innerHTML = (`${numb} is not a prime number`);
+        }
+    }
+
+    if (isPrime === true) {
+        document.querySelector(`.prime`).innerHTML = (`${numb} is a prime number`);
+    }
+}
