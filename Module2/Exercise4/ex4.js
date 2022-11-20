1c7f217a3f1494c09f0bdd2bf135bf4e98ea4b0d
kiianaaa <kiana.aghajani@metropolia.fi>
2022-11-20 15:50:48 +0200

    Done

--- Module2/Exercise4/ex4.js
+++ Module2/Exercise4/ex4.js
@@ -1,1 +1,12 @@
 'use strict';
+
+let numbers = [];
+let numb = 1
+
+for (let i = 0; numb > 0; i++) {
+    numb = parseInt(prompt("Enter a number until u dont whant to, then enter zero:"));
+    numbers.push(numb);
+}
+
+numbers.sort(function (a, b) { return b - a; });
+console.log(numbers);
