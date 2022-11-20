efe3de95236d4103922bb4c7b55d40450a33607e
kiianaaa <kiana.aghajani@metropolia.fi>
2022-11-20 15:31:31 +0200

    Done

--- Module2/Exercise3/ex3.js
+++ Module2/Exercise3/ex3.js
@@ -1,1 +1,20 @@
 'use strict';
+
+let dogs = [];
+
+for (let i = 0; i < 7; i++) {
+    let name = prompt(`What is this dog's name?`);
+    dogs.push(name);
+}
+
+dogs.sort();
+dogs.reverse();
+
+var el = document.getElementById('dog');
+var ol = document.createElement('ul');
+el.appendChild(ol);
+dogs.forEach(function (dog) {
+  var f = document.createElement('li');
+  f.appendChild(document.createTextNode(dog));
+  ol.appendChild(f);
+});
