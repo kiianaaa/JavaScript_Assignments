6b035dae3b3b11541cfc4cecbc71714294bbd589
kiianaaa <kiana.aghajani@metropolia.fi>
2022-11-23 22:24:19 +0200

    Done

--- Module2/Exercise5/ex5.js
+++ Module2/Exercise5/ex5.js
@@ -1,1 +1,15 @@
 'use strict';
+
+let numbers = [];
+
+while (true) {
+    let number = parseInt(prompt("Enter a number:"));
+    if (numbers.includes(number)) {
+        numbers.sort(function (a, b) { return a - b; });
+        console.log(numbers);
+        break;
+    }
+    else {
+        numbers.push(number);
+    }
+}
