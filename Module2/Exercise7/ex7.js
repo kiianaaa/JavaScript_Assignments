ead3549051444e6fd02dd73b11e48b879394a22f
kiianaaa <kiana.aghajani@metropolia.fi>
2022-11-26 23:17:09 +0200

    Done

--- Module2/Exercise7/ex7.js
+++ Module2/Exercise7/ex7.js
@@ -1,1 +1,20 @@
 'use strict';
+
+
+let rolls = [];
+const ul = document.createElement('ul');
+let side = prompt(`how many sides do you want?`)
+
+function rolling(side) {
+    return Math.floor(Math.random() * side) +1;
+}
+
+while (rolls.includes(side) !== true) {
+    let rollin = rolling();
+    rolls.push(rollin);
+    ul.innerHTML += `<li> ${rollin} </li>`;
+    }
+
+
+document.body.appendChild(ul);
+
