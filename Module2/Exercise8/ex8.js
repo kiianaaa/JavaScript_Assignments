ed3bdca64297efed9f731301649b5f29dfab3136
kiianaaa <kiana.aghajani@metropolia.fi>
2022-11-26 23:59:05 +0200

    Done

--- Module2/Exercise8/ex8.js
+++ Module2/Exercise8/ex8.js
@@ -1,1 +1,15 @@
 'use strict';
+
+
+let array = [`pat`, `pran`, `tharn`, `type`];
+let result = '';
+
+
+function concat(array) {
+    for (let i = 0; i < array.length; i++)
+        result = result.concat(array[i]);
+    return result;
+}
+
+
+document.querySelector(`.list`).innerHTML = concat(array);
