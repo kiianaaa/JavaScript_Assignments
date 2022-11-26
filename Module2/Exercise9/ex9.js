1e958f654760f884f349a957b0c9bfcc1ec745b5
kiianaaa <kiana.aghajani@metropolia.fi>
2022-11-27 00:25:21 +0200

    Done

--- Module2/Exercise9/ex9.js
+++ Module2/Exercise9/ex9.js
@@ -1,1 +1,20 @@
 'use strict';
+
+
+
+const numbers = [7, 10, 15, 8, 13, 18, 6];
+
+function even(numbers) {
+    const evenNumb = [];
+    for (let i = 0; i < numbers.length; i++) {
+        const num = numbers[i];
+        if (num % 2 === 0) {
+            evenNumb.push(num);
+        }
+    }
+    return evenNumb;
+}
+
+
+console.log(numbers);
+console.log(even(numbers))
