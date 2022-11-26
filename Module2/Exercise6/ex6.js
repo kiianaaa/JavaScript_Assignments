93fc815fc73c3d919c3f4ceff4e6ca0ae445aff4
kiianaaa <kiana.aghajani@metropolia.fi>
2022-11-26 22:45:46 +0200

    Done

--- Module2/Exercise6/ex6.js
+++ Module2/Exercise6/ex6.js
@@ -1,1 +1,18 @@
 'use strict';
+
+
+let rolls = [];
+const ul = document.createElement('ul');
+
+function rolling() {
+    return Math.floor(Math.random() * 6) +1;
+}
+
+while (rolls.includes(6) !== true) {
+    let rollin = rolling();
+    rolls.push(rollin);
+    ul.innerHTML += `<li> ${rollin} </li>`;
+    }
+
+
+document.body.appendChild(ul);
