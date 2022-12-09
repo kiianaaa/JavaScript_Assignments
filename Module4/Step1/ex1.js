18261c31c547f38a1fabb942390cc4f7ffe438a9
kiianaaa <kiana.aghajani@metropolia.fi>
2022-12-09 23:37:03 +0200

    Done

--- Module4/Step1/ex1.js
+++ Module4/Step1/ex1.js
@@ -1,1 +1,22 @@
 'use strict';
+
+
+fetch('https://api.tvmaze.com/search/shows?q=eye').
+
+    then(response => response.json()).
+    then(data => info(data));
+
+const p = document.getElementById('hadaf');
+
+
+
+function info(data) {
+    console.log(`Name of the show:`)
+    console.log(data[0]['show']['name']);
+    console.log(`Link to the show:`)
+    console.log(data[0]['show']['url']);
+    console.log(`Showes summery:`)
+    console.log(data[0]['show']['summary']);
+    console.log(`Show's poster:`)
+    console.log(data[0]['show']['image']['medium']);
+}
--- Module4/Step2/ex2.js
+++ Module4/Step2/ex2.js
@@ -1,1 +1,31 @@
 'use strict';
+
+
+
+fetch('https://api.tvmaze.com/search/shows?q=girls').
+
+    then(response => response.json()).
+    then(data => info(data));
+
+const p = document.getElementById('hadaf1');
+
+function info(data) {
+    document.getElementById('hadaf').innerHTML = data[0]['show']['name'];
+    const k = document.createElement('a');
+    p.appendChild(k);
+    const attr = document.createAttribute('href');
+    attr.value = data[0]['show']['url'];
+    document.querySelector('a').setAttributeNode(attr);
+    const kim = document.createAttribute('get')
+    kim.value = '_blank'
+    document.querySelector('a').setAttributeNode(kim)
+    document.querySelector('a').innerHTML = data[0]['show']['url'];
+    const img = document.createElement('img');
+    img.src = data[0]['show']['image']['medium'];
+    img.alt = `Show's poster`
+    document.getElementById('hadaf2').appendChild(img)
+    document.getElementById('hadaf3').innerHTML = data[0]['show']['summary'];
+    
+    console.log(data);
+}
+
