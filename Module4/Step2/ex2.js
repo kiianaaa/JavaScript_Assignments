fc4acdc35a3df076d583f0aebb5c7bb77b5bfd7d
kiianaaa <kiana.aghajani@metropolia.fi>
2022-12-10 00:26:17 +0200

    Done

--- Module4/Step2/ex2.js
+++ Module4/Step2/ex2.js
@@ -8,20 +8,24 @@ fetch('https://api.tvmaze.com/search/shows?q=semantic').
 const p = document.getElementById('hadaf1');
 
 function info(data) {
-    console.log(data);
+    //name
     document.getElementById('hadaf').innerHTML = data[0]['show']['name'];
-    const ab = document.createElement('a');
-    p.appendChild(ab);
-    const attr = document.createAttribute('href');
-    attr.value = data[0]['show']['url'];
-    document.querySelector('a').setAttributeNode(attr);
-    const bal = document.createAttribute('hadaf')
-    bal.value = '_blank'
-    document.querySelector('a').setAttributeNode(bal)
+    //lnk
+    const k = document.createElement('a');
+    p.appendChild(k);
+    const soo = document.createAttribute('href');
+    soo.value = data[0]['show']['url'];
+    document.querySelector('a').setAttributeNode(soo);
+    const kim = document.createAttribute('hadaf')
+    // blank opens in new tab
+    kim.value = '_blank'
+    document.querySelector('a').setAttributeNode(kim)
     document.querySelector('a').innerHTML = data[0]['show']['url'];
+    //poster
     const img = document.createElement('img');
     img.src = data[0]['show']['image']['medium'];
     img.alt = `Show's poster`
     document.getElementById('hadaf2').appendChild(img)
+    //summery
     document.getElementById('hadaf3').innerHTML = data[0]['show']['summary'];
 }
